7a9697a7f20cc2983fdaaa52d74c1ea9328a9de2
Haseeb Abbas <Haseebabbas0004@gmail.com>
2021-05-31 07:30:59 +0500

    Swipe Delete/Search/Move/

--- app/src/main/java/com/devname/doo/CustomAdapterR.java
+++ app/src/main/java/com/devname/doo/CustomAdapterR.java
@@ -1,9 +1,9 @@
 package com.devname.doo;
 
-import android.content.res.Resources;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,25 +11,62 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.devname.doo.databinding.TaskItemBinding;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
-public class CustomAdapterR extends RecyclerView.Adapter<CustomAdapterR.ViewHolder> {
+public class CustomAdapterR extends RecyclerView.Adapter<CustomAdapterR.ViewHolder> implements Filterable {
 
     // Getting Data List
     private final ArrayList<TaskItem> taskItemArrayList;
+    private final ArrayList<TaskItem> completeList;
+
+    public void updateList(){
+        completeList.clear();
+        completeList.addAll(taskItemArrayList);
+    }
+    // Filtering
+    private final Filter filter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<TaskItem> filteredList = new ArrayList<>();
+            if (constraint.toString().isEmpty()){
+                filteredList.addAll(completeList);
+            } else {
+                for (TaskItem taskItem :
+                        completeList) {
+                    if (taskItem.getTask().toLowerCase().contains(constraint.toString().toLowerCase()))
+                        filteredList.add(taskItem);
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            taskItemArrayList.clear();
+            taskItemArrayList.addAll((Collection<TaskItem>) results.values);
+            notifyDataSetChanged();
+        }
+    };
+
     public CustomAdapterR(ArrayList<TaskItem> taskItemArrayList) {
         this.taskItemArrayList = taskItemArrayList;
+        this.completeList = new ArrayList<>(taskItemArrayList);
     }
 
     // Creating Listener Interface
+/*
     private onItemClickListener listener;
 
     public interface onItemClickListener {
-        void onDeleteClick(View view, int position);
+        void onDeleteClick(int position);
     }
 
     public void setOnItemClickListener(onItemClickListener listener){
         this.listener = listener;
     }
+*/
 
 
     // Adapter Overridden Methods
@@ -42,30 +79,17 @@ public class CustomAdapterR extends RecyclerView.Adapter<CustomAdapterR.ViewHold
     @Override
     public void onBindViewHolder(@NonNull CustomAdapterR.ViewHolder holder, int position) {
         TaskItem currentData = taskItemArrayList.get(position);
-        holder.setDetails(currentData, listener);
-
-        if (position + 1 == getItemCount()) {
-            // It is the last item of the list
-            // Set bottom margin to 72dp
-            setBottomMargin(holder.itemView, (int) (72 * Resources.getSystem().getDisplayMetrics().density));
-        } else {
-            // Reset bottom margin back to zero
-            setBottomMargin(holder.itemView, 4);
-        }
-
-    }
-
-    private static void setBottomMargin(View view, int bottomMargin) {
-        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
-            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
-            params.setMargins(params.leftMargin, params.topMargin, params.rightMargin, bottomMargin);
-            view.requestLayout();
-        }
+        holder.setDetails(currentData);
     }
 
     @Override
     public int getItemCount() {
         return taskItemArrayList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
     }
 
 
@@ -78,18 +102,9 @@ public class CustomAdapterR extends RecyclerView.Adapter<CustomAdapterR.ViewHold
             this.itemBinding = itemBinding;
         }
 
-        public void setDetails(TaskItem taskItem, onItemClickListener listener){
-            itemBinding.taskView.setText(taskItem.getTask());
+        public void setDetails(TaskItem taskItem){
+            itemBinding.expandableTaskView.setText(taskItem.getTask());
             itemBinding.dateView.setText(taskItem.getDate());
-            // Triggers the above method of customClickListener
-            itemBinding.deleteTaskItem.setOnClickListener(v -> {
-                if (listener != null) {
-                    int position = getAdapterPosition();
-                    if (position != RecyclerView.NO_POSITION) {
-                        listener.onDeleteClick(itemView, position);
-                    }
-                }
-            });
         }
     }
 }
--- app/src/main/java/com/devname/doo/MainActivity.java
+++ app/src/main/java/com/devname/doo/MainActivity.java
@@ -1,11 +1,18 @@
 package com.devname.doo;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,35 +20,74 @@ import com.devname.doo.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 import maes.tech.intentanim.CustomIntent;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final ArrayList<TaskItem> taskItemArrayList = new ArrayList<>();
     private CustomAdapterR customAdapter;
     private ActivityMainBinding mainBinding;
     private ActivityResultLauncher<Intent> taskActivityResultLauncher;
-    private final ArrayList<TaskItem> taskItemArrayList = new ArrayList<>();
+    private ItemTouchHelper itemTouchHelper;
+
+    //Swipe gesture creation
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback( ItemTouchHelper.UP |
+            ItemTouchHelper.DOWN, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            int fromPosition = viewHolder.getAdapterPosition();
+            int toPosition = target.getAdapterPosition();
+
+            Collections.swap(taskItemArrayList, fromPosition, toPosition);
+            customAdapter.notifyItemMoved(fromPosition, toPosition);
+            customAdapter.updateList();
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+            TaskItem taskItem = taskItemArrayList.get(position);
+            removeTask(position);
+            showUndoSnackBar(position, taskItem);
+        }
+
+        // snackBar Undo Creation
+        private void showUndoSnackBar(int position, TaskItem taskItem) {
+            Snackbar snackbar = Snackbar.make(mainBinding.getRoot(), "Task Deleted", Snackbar.LENGTH_SHORT)
+                    .setAction("Undo", v -> addTask(position, taskItem))
+                    .setActionTextColor(getResources().getColor(R.color.secondaryColor));
+            snackbar.setAnchorView(mainBinding.fabBtn);
+            snackbar.show();
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(getResources().getColor(R.color.secondaryColor))
+                    .addSwipeLeftLabel("Delete")
+                    .addSwipeLeftActionIcon(R.drawable.ic_sharp_delete)
+                    .setSwipeLeftActionIconTint(Color.WHITE)
+                    .setSwipeLeftLabelColor(Color.WHITE)
+                    .create()
+                    .decorate();
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
-
-        TaskItem task = new TaskItem("This is Example Task","date will be whatever I want");
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
-        taskItemArrayList.add(task);
+        setSupportActionBar(mainBinding.toolbar);
+//      Dummy Data Initialization
+        for (int i = 0; i < 30; i++) {
+            taskItemArrayList.add(new TaskItem("This is Example Task" + i, i + "/05/2021"));
+        }
 
         mainBinding.fabBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, TaskDetailActivity.class);
@@ -58,40 +104,78 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         taskActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == RESULT_OK) {
-                addTask(result.getData());
-                showSnackBar("Task Added Successfully");
-            } else if (result.getResultCode() == RESULT_CANCELED) {
-                mainBinding.recyclerView.scrollToPosition(0);
-                showSnackBar("Task Canceled");
-            }
+                TaskItem taskItem = result.getData().getParcelableExtra("KEY");
+                addTask(0, taskItem);
+                showSnackBar(getString(R.string.task_added_successfully));
+            } else if (result.getResultCode() == RESULT_CANCELED)
+                showSnackBar(getString(R.string.task_canceled));
         });
     }
+
 
     private void buildRecyclerView() {
         customAdapter = new CustomAdapterR(taskItemArrayList);
 
-        customAdapter.setOnItemClickListener((view, position) -> removeTask(position));
+//        customAdapter.setOnItemClickListener(this::removeTask);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         mainBinding.recyclerView.setLayoutManager(layoutManager);
         mainBinding.recyclerView.setAdapter(customAdapter);
+
+        // Swipe Delete
+        itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(mainBinding.recyclerView);
     }
 
     private void removeTask(int position) {
         taskItemArrayList.remove(position);
         customAdapter.notifyItemRemoved(position);
+        customAdapter.updateList();
     }
 
-    private void addTask(Intent data){
-        TaskItem taskItem = data.getParcelableExtra("KEY");
-        taskItemArrayList.add(0,taskItem);
-        customAdapter.notifyItemInserted(0);
-        mainBinding.recyclerView.scrollToPosition(0);
+    private void addTask(int position, TaskItem taskItem) {
+        taskItemArrayList.add(position, taskItem);
+        customAdapter.notifyItemInserted(position);
+        if (position == 0 || position == customAdapter.getItemCount()-1)
+            mainBinding.recyclerView.scrollToPosition(position);
+        customAdapter.updateList();
     }
 
-    private void showSnackBar(String string){
+    private void showSnackBar(String string) {
         Snackbar snackbar = Snackbar.make(mainBinding.getRoot(), string, Snackbar.LENGTH_SHORT);
         snackbar.setAnchorView(mainBinding.fabBtn);
         snackbar.show();
+    }
+
+    // On Options creation
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_btn);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setQueryHint(getString(R.string.search_));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                customAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        searchView.setOnQueryTextFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus){
+                mainBinding.fabBtn.hide();
+                itemTouchHelper.attachToRecyclerView(null);
+            } else {
+                mainBinding.recyclerView.scrollToPosition(0);
+                mainBinding.fabBtn.show();
+                itemTouchHelper.attachToRecyclerView(mainBinding.recyclerView);
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
